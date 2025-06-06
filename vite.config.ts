88befc4bb5b39f55c1f22b2b8f440713b4551840
Gregorio Amano III <66071367+geeOnama940515@users.noreply.github.com>
2025-06-06 13:51:51 +0800

    Updated docker-compose.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   preview: {
-    allowedHosts: ['barn-demo.gregdoesdev.xyz'],
+    host: true,
+    port: 9007,
+    strictPort: true,
   },
 });
