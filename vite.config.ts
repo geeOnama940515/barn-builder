44096ee7db4bac6276f6d66f13376597cb270fde
Gregorio Amano III <66071367+geeOnama940515@users.noreply.github.com>
2025-06-06 13:39:59 +0800

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   preview: {
+    host: '0.0.0.0',
+    port: 9007,
+    strictPort: true,
     allowedHosts: ['barn-demo.gregdoesdev.xyz'],
   },
 });
